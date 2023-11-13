c78da004d208088a471390d701ef26d9643146a3
Carolista <cjones.renaissancewoman@gmail.com>
2023-11-13 16:53:33 -0500

    added solution for unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,33 @@
-
 let launchcode = {
-  
+	organization: "nonprofit",
+	executiveDirector: "Jeff",
+	percentageCoolEmployees: 100,
+	programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+	launchOutput: function (num) {
+		let parts = {
+			2: "Launch",
+			3: "Code",
+			5: "Rocks"
+		};
+		let result = "";
+		for (let modulus in parts) {
+			if (num % Number(modulus) === 0) {
+				if (modulus == 5 && result.length > 0) {
+					result += " ";
+				}
+				result += parts[modulus];
+			}
+		}
+		if (result === "") {
+			result = "Rutabagas! That doesn't work.";
+		} else {
+			result += "!";
+		}
+		if (result === "Launch Rocks!") {
+			result += " (CRASH!!!!)";
+		}
+		return result;
+	}
 }
 
 module.exports = launchcode;
-
