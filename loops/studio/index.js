7b2eed6bef55cbf3397e3d1f5204c89026f2955e
Carolista <cjones.renaissancewoman@gmail.com>
2024-03-15 03:45:58 -0400

    added solution to chapter 9 studio

--- loops/studio/index.js
+++ loops/studio/index.js
@@ -1,3 +1,7 @@
 const shuttleManagement = require('./solution.js');
 
 shuttleManagement.runProgram();
+
+// Write your code only in solution.js
+// Use the command 'node index' from the terminal in the loops/studio directory to run program
+// Use the command 'npm test' to run unit tests
