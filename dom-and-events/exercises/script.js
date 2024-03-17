0ba597511d156117c50041df8d50007b0a2a80b7
Carolista <cjones.renaissancewoman@gmail.com>
2024-03-17 17:01:24 -0400

    added solution for chapter 23 exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,27 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const abortButton = document.getElementById('abortMission');
+  const takeOffButton = document.getElementById('liftoffButton');
+  const status = document.getElementById('statusReport');
 
-    // Put your code for the exercises here.
-    
+  // Put your code for the exercises here.
+  takeOffButton.addEventListener('click', function () {
+    status.innerHTML = 'Houston, we have liftoff!';
+  });
+
+  abortButton.addEventListener('mouseover', function () {
+    abortButton.style.backgroundColor = 'red';
+  });
+
+  abortButton.addEventListener('mouseout', function () {
+    abortButton.style.backgroundColor = '';
+  });
+
+  abortButton.addEventListener("click", function () {
+    let shouldAbort = window.confirm("Are you sure you want to abort the mission?");
+    if (shouldAbort) {
+      status.innerHTML = "Mission aborted! Space shuttle returning home.";
+    }
+  });
 }
 
-window.addEventListener("load", init);
+window.addEventListener('load', init);
