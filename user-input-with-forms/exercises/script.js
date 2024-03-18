e43f79f7dc70c707efc533c1584a590a206dd560
Carolista <cjones.renaissancewoman@gmail.com>
2024-03-17 21:44:12 -0400

    added solution for chapter 25 exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,19 @@
 //Code Your Solution Below
+window.addEventListener('load', function () {
+  const form = document.querySelector('form');
+
+  const nameInput = document.querySelector('input[name=testName]');
+  const dateInput = document.querySelector('input[name=testDate]');
+  const boosterCountInput = document.querySelector('input[name=boosterCount]');
+  const windRatingInput = document.querySelector('input[name=windRating]');
+
+  form.addEventListener('submit', function (event) {
+    if (nameInput.value.trim() === ""
+      || dateInput.value === ""
+      || boosterCountInput.value === ""
+      || windRatingInput.value === "") {
+        event.preventDefault();
+      }
+  });
+
+});
